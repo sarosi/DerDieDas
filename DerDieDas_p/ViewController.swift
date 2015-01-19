160913d5170aadf5ed7da70551622a2dad014df4
Gabor Silber <silber@Gabors-MacBook-Pro.local>
2015-01-19 17:12:46 +0100

    Initial Commit

---
+++ DerDieDas_p/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DerDieDas_p
+//
+//  Created by Gabor Silber on 19/01/15.
+//  Copyright (c) 2015 Gabor Silber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
